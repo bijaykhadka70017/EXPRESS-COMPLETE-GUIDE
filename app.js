e1f8d78f28e315df32c6054d531be278e1e84712
Bijay Khadka <bijaykhadka@Bijays-MacBook-Pro.local>
2026-06-07 14:36:06 -0300

    153. Syncing JS definitions to the database

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const sequelize = require('./util/database');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,4 +23,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+// Sync all defined models to the database
+sequelize.sync()
+    .then(result => {
+        // console.log(result); // Returns a complex setup metadata object
+        app.listen(3000);    // Only start the server if the DB sync succeeds
+    })
+    .catch(err => {
+        console.log(err);
+    });
