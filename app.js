ec4bbb01eeb7671a5def57c97ef94b81299435d9
Bijay Khadka <bijaykhadka250@gmail.com>
2026-06-08 13:12:26 -0300

    Adding one to many relationship

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,8 +25,17 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Direction A: A product belongs to a specific user (Sets up the connection)
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+
+// Direction B: The inverse relationship (Clarifies the structural flow)
+User.hasMany(Product);
+
 // Sync all defined models to the database
-sequelize.sync()
+sequelize.sync({ force: true })
     .then(result => {
         // console.log(result); // Returns a complex setup metadata object
         app.listen(3000);    // Only start the server if the DB sync succeeds
